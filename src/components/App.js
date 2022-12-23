16cf212dc9eae9eed5bfab63fecdaf5a712cc568
Mouktika Namde <mouktikanamde712@gmail.com>
2022-12-22 23:44:51 -0800

    Deploy Posts in Home component using hooks and map() and add a loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getPosts } from '../api';
 import { Home } from '../pages/index';
+import { Loader } from './';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await getPosts();
       console.log(response);
+
+      if (response.success) {
+        //see the console.log statement above to see the response object structure
+        setPosts(response.data.posts);
+      }
+
+      //after getting the posts, set the loading to false
+      setLoading(false);
     };
 
     fetchPosts();
   }, []);
+
+  //after posts get loaded setLoading will be set to false so the page will get loaded again(just like setState) OR due to useEffect hook??
+  if (loading) {
+    return <Loader />;
+  }
+
+  // at that time the Home component will get rendered
   return (
     <div className="App">
-      <Home/>
+      <Home posts={posts} />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,59 @@
 import styles from '../styles/home.module.css';
 
-const Home = () => {
+//get the props and destructure it right there
+const Home = ({ posts }) => {
   return (
     <div className={styles.postsList}>
-      <div className={styles.postWrapper}>
-        <div className={styles.postHeader}>
-          <div className={styles.postAvatar}>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-              alt="user-pic"
-            />
-            <div>
-              <span className={styles.postAuthor}>Mouktika</span>
-              <span className={styles.postTime}>a minute ago</span>
-            </div>
-          </div>
-          <div className={styles.postContent}>Post Conetnt</div>
-
-          <div className={styles.postActions}>
-            <div className={styles.postLike}>
+      {posts.map((post, index) => (
+        <div className={styles.postWrapper} key={index}>
+          <div className={styles.postHeader}>
+            <div className={styles.postAvatar}>
               <img
-                src="https://cdn-icons-png.flaticon.com/512/2589/2589175.png"
-                alt="likes-icon"
+                src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                alt="user-pic"
               />
-              <span>5</span>
+              <div>
+                <span className={styles.postAuthor}>{post.user.name}</span>
+                <span className={styles.postTime}>a minute ago</span>
+              </div>
             </div>
+            <div className={styles.postContent}>{post.content}</div>
 
-            <div className={styles.postCommentsIcon}>
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/2190/2190552.png"
-                alt="comments-icon"
-              />
-              <span>2</span>
-            </div>
-          </div>
-          <div className={styles.postCommentBox}>
-            <input placeholder="Start typing a comment" />
-          </div>
-
-          <div className={styles.postCommentsList}>
-            <div className={styles.postCommentsItem}>
-              <div className={styles.postCommentHeader}>
-                <span className={styles.postCommentAuthor}>Bill</span>
-                <span className={styles.postCommentTime}>a minute ago</span>
-                <span className={styles.postCommentLikes}>22</span>
+            <div className={styles.postActions}>
+              <div className={styles.postLike}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/2589/2589175.png"
+                  alt="likes-icon"
+                />
+                <span>{post.likes.length}</span>
               </div>
 
-              <div className={styles.postCommentContent}>Random comment</div>
+              <div className={styles.postCommentsIcon}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/2190/2190552.png"
+                  alt="comments-icon"
+                />
+                <span>{post.comments.length}</span>
+              </div>
+            </div>
+            <div className={styles.postCommentBox}>
+              <input placeholder="Start typing a comment" />
+            </div>
+
+            <div className={styles.postCommentsList}>
+              <div className={styles.postCommentsItem}>
+                <div className={styles.postCommentHeader}>
+                  <span className={styles.postCommentAuthor}>Bill</span>
+                  <span className={styles.postCommentTime}>a minute ago</span>
+                  <span className={styles.postCommentLikes}>22</span>
+                </div>
+
+                <div className={styles.postCommentContent}>Random comment</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
